4dfbec4203e5ff848831e48b098067e0716b63dd
Praveen <lunavathpraveen36@gmail.com>
2026-02-02 23:07:50 +0530

    Update profile image path in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import profile from "../assets/profile.png";
+import profile from "/Passport_photo.jpeg";
 
 function Hero({ darkMode }) {
   return (
